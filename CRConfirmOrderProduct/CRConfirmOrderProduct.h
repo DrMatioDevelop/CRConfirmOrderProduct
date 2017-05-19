9dc5f357a0f14210a7fd80eb0d7f63cd0a0745ad
d2c_cyf <cyfsoftware@126.com>
2017-05-19 16:02:31 +0800

    Initial Commit

---
+++ CRConfirmOrderProduct/CRConfirmOrderProduct.h
@@ -0,0 +1,19 @@
+//
+//  CRConfirmOrderProduct.h
+//  CRConfirmOrderProduct
+//
+//  Created by d2c_cyf on 17/5/19.
+//  Copyright © 2017年 d2c_cyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRConfirmOrderProduct.
+FOUNDATION_EXPORT double CRConfirmOrderProductVersionNumber;
+
+//! Project version string for CRConfirmOrderProduct.
+FOUNDATION_EXPORT const unsigned char CRConfirmOrderProductVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRConfirmOrderProduct/PublicHeader.h>
+
+
